c4a4ab7bb7dd6b36d598f92e759e75549a47d156
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-19 16:48:40 -0300

    se prepara AuthStore para mejora

--- store/authStore.js
+++ store/authStore.js
@@ -1,7 +1,32 @@
 import { create } from "zustand";
+import { getToken, removeToken, setToken } from "../src/utils/token";
 // https://starter.obytes.com/guides/authentication/
+
 export const useAuthStore = create((set) => ({
   isAuthenticated: false,
+  status: 'idle', // reemplazara a "isAuthenticated"
+  token: null, // reemplazara a "isAuthenticated"
   login: () => set({ isAuthenticated: true }),
   logout: () => set({ isAuthenticated: false }),
+
+  signIn: (token) => { // reemplazara a "login"
+    setToken(token);
+    set({ status: 'signIn', token });
+  },
+  signOut: () => { // reemplazara a "logout"
+    removeToken();
+    set({ status: 'signOut', token: null });
+  },
+  hydrate: () => {
+    try {
+      const userToken = getToken();
+      if (userToken !== null) {
+        get().signIn(userToken);
+      } else {
+        get().signOut();
+      }
+    } catch (e) {
+      // Manejo de errores
+    }
+  },
 }));
