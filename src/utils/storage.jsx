d7f7e496898a3274af560cad787ec0d9a2d01bc0
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-20 00:15:03 -0300

    actualizacion de storage

--- src/utils/storage.jsx
+++ src/utils/storage.jsx
@@ -1,17 +1,44 @@
-import { MMKV } from "react-native-mmkv";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// para mayor rendimeinto luego se peude variar a react-native-mmkv
 
-// gestion de almacenamiento de datos similar a local-storage
-export const storage = new MMKV();
+/**
+ * Obtener un valor de AsyncStorage
+ * @param {string} key - Clave del dato a recuperar
+ * @returns {Promise<any>} - Valor almacenado o `null` si no existe
+ */
+export const getItem = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null; // Intenta deserializar objetos
+  } catch (error) {
+    console.error("Error al obtener el dato:", error);
+    return null;
+  }
+};
 
-export function getItem(key) {
-  const value = storage.getString(key);
-  return value ? JSON.parse(value) || null : null
-}
+/**
+ * Guardar un valor en AsyncStorage
+ * @param {string} key - Clave para almacenar el dato
+ * @param {any} value - Valor a almacenar
+ */
+export const setItem = async (key, value) => {
+  try {
+    const serializedValue =
+      typeof value === "object" ? JSON.stringify(value) : value.toString();
+    await AsyncStorage.setItem(key, serializedValue);
+  } catch (error) {
+    console.error("Error al guardar el dato:", error);
+  }
+};
 
-export async function setItem(key, value) {
-  storage.set(key, JSON.stringify(value));
-}
-
-export async function removeItem(key) {
-  storage.delete(key);
-}
+/**
+ * Eliminar un valor de AsyncStorage
+ * @param {string} key - Clave del dato a eliminar
+ */
+export const removeItem = async (key) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (error) {
+    console.error("Error al eliminar el dato:", error);
+  }
+};
