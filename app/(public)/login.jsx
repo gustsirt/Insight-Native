dff79c13f824d0b52e21ad3246e64d5f120654b4
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-22 19:44:42 -0300

    ✖ Login Page

--- app/(public)/login.jsx
+++ app/(public)/login.jsx
@@ -1,24 +1,13 @@
-import { Pressable, Text, View } from 'react-native'
+
 import React from 'react'
-import { Link } from 'expo-router'
-import { useAuthStore } from '../../store/authStore'
+import LoginForm from '../../src/modules/auth/LoginForm'
+import Frame from '../../src/ui/box/Frame'
 
 export default function Login() {
-  const { signIn } = useAuthStore();
-
-  const handleLogin = () => {
-    signIn("token_prueba");
-  };
 
   return (
-    <View className="flex items-center justify-center flex-1">
-      <Text>Pantalla de Login 😁</Text>
-      <Link href="/" asChild><Pressable>
-        <Text className='p-4 text-xl text-blue-500'>Home</Text>
-      </Pressable></Link>
-      <Pressable onPress={handleLogin}>
-        <Text className='p-4 text-xl text-blue-500'>Login</Text>
-      </Pressable>
-    </View>
+    <Frame>
+      <LoginForm />
+    </Frame>
   )
 }
